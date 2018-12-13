9edea6be6f70a0177108e2e7cfb3969f4d620ec1
Walker Lindley <walker@walkerlindley.com>
2018-12-12 21:58:32 -0800

    Formatting

--- sixth/one/src/main.rs
+++ sixth/one/src/main.rs
@@ -1,7 +1,7 @@
+use std::cmp;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::cmp;
 
 fn main() {
     let points = load_points();
@@ -15,7 +15,7 @@ struct Point {
 
 impl Point {
     fn new(x: i32, y: i32) -> Point {
-        return Point{x, y};
+        return Point { x, y };
     }
 }
 
@@ -27,41 +27,43 @@ struct Rect {
 
 impl Rect {
     fn new(x: i32, y: i32, x2: i32, y2: i32) -> Rect {
-        return Rect{
+        return Rect {
             topLeft: Point::new(x, y),
             bottomRight: Point::new(x2, y2),
         };
     }
 
     fn enclosing(points: &Vec<Point>) -> Rect {
-        return points
-            .iter()
-            .fold(Rect::new(std::i32::MAX, std::i32::MAX, std::i32::MIN, std::i32::MIN),
-                  |rect, p| {
-                      let min_x = cmp::min(rect.topLeft.x, p.x);
-                      let min_y = cmp::min(rect.topLeft.y, p.y);
-                      let max_x = cmp::max(rect.bottomRight.x, p.x);
-                      let max_y = cmp::max(rect.bottomRight.y, p.y);
-                      return Rect{
-                          topLeft: Point::new(min_x, min_y),
-                          bottomRight: Point::new(max_x, max_y),
-                      }
-                  });
+        return points.iter().fold(
+            Rect::new(std::i32::MAX, std::i32::MAX, std::i32::MIN, std::i32::MIN),
+            |rect, p| {
+                let min_x = cmp::min(rect.topLeft.x, p.x);
+                let min_y = cmp::min(rect.topLeft.y, p.y);
+                let max_x = cmp::max(rect.bottomRight.x, p.x);
+                let max_y = cmp::max(rect.bottomRight.y, p.y);
+                return Rect {
+                    topLeft: Point::new(min_x, min_y),
+                    bottomRight: Point::new(max_x, max_y),
+                };
+            },
+        );
     }
 }
 
 fn load_points() -> Vec<Point> {
     let f = File::open("input.txt").expect("could not find file");
     let r = BufReader::new(&f);
-    return r.lines()
+    return r
+        .lines()
         .map(|l| l.unwrap())
         .map(|l| {
             let mut tokens = l.split(|c| c == ',').map(|t| t.trim());
-            return Point{
+            return Point {
                 x: tokens.next().unwrap().parse().unwrap(),
                 y: tokens.next().unwrap().parse().unwrap(),
             };
-        }).collect();
+        })
+        .collect();
 }
 
 #[cfg(test)]
@@ -70,9 +72,26 @@ mod tests {
 
     #[test]
     fn test_rect_enclosing() {
-        assert_eq!(Rect::new(0, 0, 0, 0), Rect::enclosing(&vec![Point::new(0, 0)]));
-        assert_eq!(Rect::new(0, 0, 2, 2), Rect::enclosing(&vec![Point::new(0, 0), Point::new(2, 2)]));
-        assert_eq!(Rect::new(1, 2, 3, 4), Rect::enclosing(&vec![Point::new(3, 4), Point::new(1, 2)]));
-        assert_eq!(Rect::new(1, 2, 3, 4), Rect::enclosing(&vec![Point::new(3, 2), Point::new(1, 4)]));
+        assert_eq!(
+            Rect::new(0, 0, 0, 0),
+            Rect::enclosing(&vec![Point::new(0, 0)])
+        );
+        assert_eq!(
+            Rect::new(0, 0, 2, 2),
+            Rect::enclosing(&vec![Point::new(0, 0), Point::new(2, 2)])
+        );
+        assert_eq!(
+            Rect::new(1, 2, 3, 4),
+            Rect::enclosing(&vec![Point::new(3, 4), Point::new(1, 2)])
+        );
+        assert_eq!(
+            Rect::new(1, 2, 3, 4),
+            Rect::enclosing(&vec![Point::new(3, 2), Point::new(1, 4)])
+        );
+    }
+
+    #[test]
+    fn test_points() {
+        unimplemented!();
     }
 }
