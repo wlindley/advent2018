8482c1e84dbf6061639be473adb3f9c73bd9b40d
Walker Lindley <walker@walkerlindley.com>
2018-12-13 23:38:46 -0800

    WIP day 6, part 1.

--- sixth/one/src/main.rs
+++ sixth/one/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let points = load_points();
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct Point {
     x: i32,
     y: i32,
@@ -17,19 +17,23 @@ impl Point {
     fn new(x: i32, y: i32) -> Point {
         return Point { x, y };
     }
+
+    fn distance(&self, other: &Point) -> i32 {
+        return (other.x - self.x).abs() + (other.y - self.y).abs();
+    }
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct Rect {
-    topLeft: Point,
-    bottomRight: Point,
+    top_left: Point,
+    bottom_right: Point,
 }
 
 impl Rect {
     fn new(x: i32, y: i32, x2: i32, y2: i32) -> Rect {
         return Rect {
-            topLeft: Point::new(x, y),
-            bottomRight: Point::new(x2, y2),
+            top_left: Point::new(x, y),
+            bottom_right: Point::new(x2, y2),
         };
     }
 
@@ -37,16 +41,45 @@ impl Rect {
         return points.iter().fold(
             Rect::new(std::i32::MAX, std::i32::MAX, std::i32::MIN, std::i32::MIN),
             |rect, p| {
-                let min_x = cmp::min(rect.topLeft.x, p.x);
-                let min_y = cmp::min(rect.topLeft.y, p.y);
-                let max_x = cmp::max(rect.bottomRight.x, p.x);
-                let max_y = cmp::max(rect.bottomRight.y, p.y);
+                let min_x = cmp::min(rect.top_left.x, p.x);
+                let min_y = cmp::min(rect.top_left.y, p.y);
+                let max_x = cmp::max(rect.bottom_right.x, p.x);
+                let max_y = cmp::max(rect.bottom_right.y, p.y);
                 return Rect {
-                    topLeft: Point::new(min_x, min_y),
-                    bottomRight: Point::new(max_x, max_y),
+                    top_left: Point::new(min_x, min_y),
+                    bottom_right: Point::new(max_x, max_y),
                 };
             },
         );
+    }
+
+    fn points(&self) -> PointIterator {
+        return PointIterator {
+            rect: self.clone(),
+            cur: self.top_left.clone(),
+        };
+    }
+}
+
+struct PointIterator {
+    rect: Rect,
+    cur: Point,
+}
+
+impl Iterator for PointIterator {
+    type Item = Point;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur.y > self.rect.bottom_right.y {
+            return Option::None;
+        }
+        let point = self.cur.clone();
+        self.cur.x += 1;
+        if self.cur.x > self.rect.bottom_right.x {
+            self.cur.x = self.rect.top_left.x;
+            self.cur.y += 1;
+        }
+        return Option::Some(point);
     }
 }
 
@@ -88,10 +121,46 @@ mod tests {
             Rect::new(1, 2, 3, 4),
             Rect::enclosing(&vec![Point::new(3, 2), Point::new(1, 4)])
         );
+        assert_eq!(
+            Rect::new(1, 2, 3, 4),
+            Rect::enclosing(&vec![Point::new(3, 2), Point::new(2, 3), Point::new(1, 4)])
+        );
     }
 
     #[test]
     fn test_points() {
-        unimplemented!();
+        assert_eq!(
+            vec![Point::new(0, 0), Point::new(1, 0)],
+            Rect::new(0, 0, 1, 0).points().collect::<Vec<Point>>()
+        );
+        assert_eq!(
+            vec![
+                Point::new(0, 0),
+                Point::new(1, 0),
+                Point::new(0, 1),
+                Point::new(1, 1)
+            ],
+            Rect::new(0, 0, 1, 1).points().collect::<Vec<Point>>()
+        );
     }
+
+    #[test]
+    fn test_distance() {
+        assert_eq!(1, Point::new(0, 0).distance(&Point::new(1, 0)));
+        assert_eq!(1, Point::new(0, 0).distance(&Point::new(0, 1)));
+        assert_eq!(2, Point::new(0, 0).distance(&Point::new(1, 1)));
+        assert_eq!(7, Point::new(1, 2).distance(&Point::new(5, -1)));
+    }
+
+    // #[test]
+    // fn test_calculate_areas() {
+    //     let areas = calculate_areas(vec![
+    //         Point::new(0, 0), //inf
+    //         Point::new(0, 8), //inf
+    //         Point::new(8, 0), //inf
+    //         Point::new(8, 8), //inf
+    //         Point::new(4, 4), //23
+    //     ]);
+    //     assert_eq!(, )
+    // }
 }
